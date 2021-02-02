7f558175e36375b1b88748976ecb76eef30b3d85
ilya-lezhnev <78431160+ilya-lezhnev@users.noreply.github.com>
2021-02-02 19:05:22 +0300

    Add files via upload

---
+++ HW1.js
@@ -0,0 +1,8 @@
+var button1 = document.querySelector("#id1")
+button1.onclick = function goToSite() {
+    document.location.href = "https://github.com/ilya-lezhnev/JS"
+}
+var button2 = document.querySelector('#id2')
+button2.onclick = function () {
+     document.body.style.background = "#228B22" // зелёный
+}
