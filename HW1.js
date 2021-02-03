a1a568c23e62168566b7023649f0a4f2608a1530
Ilya Lezhnev <78431160+ilya-lezhnev@users.noreply.github.com>
2021-02-03 08:13:16 +0300

    Update HW1.js

--- HW1.js
+++ HW1.js
@@ -3,6 +3,6 @@ button1.onclick = function goToSite() {
     document.location.href = "https://github.com/ilya-lezhnev/JS"
 }
 var button2 = document.querySelector('#id2')
-button2.onclick = function () {
+button2.onclick = function changeBackgroundColor() {
      document.body.style.background = "#228B22" // зелёный
 }
